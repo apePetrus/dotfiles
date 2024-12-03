6f73048d0b5c4aeeb80e445205fdb60e0b21fb72
apePetrus <shiveringveteran@proton.me>
2024-12-03 06:00:57 -0300

    Added gaps for dwm

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -3,6 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -131,6 +132,11 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+  /* Gaps */
+  { MODKEY,                 XK_bracketleft,  setgaps,        {.i = -1 } },
+  { MODKEY,                 XK_bracketright, setgaps,        {.i = +1 } },
+  /*{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },*/
 
   /* Tag keys */
 	TAGKEYS(                        XK_1,                      0)
