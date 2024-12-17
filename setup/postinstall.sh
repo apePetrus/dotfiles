427ddb0cb2045988d44561b4495e4491787b89a8
apePetrus <shiveringveteran@proton.me>
2024-12-17 17:07:36 -0300

    Updated postinstall script

--- setup/postinstall.sh
+++ setup/postinstall.sh
@@ -18,3 +18,6 @@ sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pip
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
+
+# .config, .themes, .icons, .xinitrc, scripts, feh-bgs and zsh files
+cp -rf ./home-dir/* $HOME
