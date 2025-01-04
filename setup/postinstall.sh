277d87e333b75b87f22a6a60432ca3a42dd8aff8
apePetrus <shiveringveteran@proton.me>
2025-01-04 10:31:25 -0300

    Updated setup script, updated dwm default keybing for browser, removed random .xintirc file that wasnt being used, updated voidpkgs.txt

--- setup/postinstall.sh
+++ setup/postinstall.sh
@@ -3,12 +3,14 @@
 # necessary pkgs
 sudo xbps-install -S $(cat ./voidpkgs.txt)
 
-# touchpad
-sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
+# daemons
+sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -s /etc/sv/elogind /var/service
+sudo ln -s /etc/sv/polkitd /var/service
 
-# zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# touchpad
+sudo mkdir -p /etc/X11/xorg.conf.d/
+sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
 
 # pipewire
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
@@ -20,4 +22,9 @@ sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 
 # .config, .themes, .icons, .xinitrc, scripts, feh-bgs and zsh files
-cp -rf ./home-dir/* $HOME
+cp -rf ./home-dir/. /home/petrus/
+
+# zsh and p10k
+mkdir -p /home/petrus/.oh-my-zsh/custom/themes
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/petrus/.oh-my-zsh/custom/themes/powerlevel10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -34,7 +34,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       0,       0,           -1 },
-	//{ "qutebrowser", NULL,    NULL,       1 << 8,       0,           -1 },
+	{ "qutebrowser", NULL,    NULL,       0,       0,           -1 },
 };
 
 /* layout(s) */
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = { "firefox", NULL };
+static const char *browsercmd[] = { "qutebrowser", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
