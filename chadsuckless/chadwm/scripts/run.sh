f7e95f3ff9f35f1a78ff67f20a415b085dd288f0
apePetrus <shiveringveteran@proton.me>
2025-02-01 21:29:04 -0300

    Reworked .xinitrc file because of chadwm run.sh setting up the first things is more containerized

--- chadsuckless/chadwm/scripts/run.sh
+++ chadsuckless/chadwm/scripts/run.sh
@@ -2,9 +2,11 @@
 
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
+feh --no-fehbg --bg-fill "$HOME/.feh-bgs/think.jpg" &
 xset r rate 200 50 &
 picom &
+setxkbmap br &
+pipewire &
 
 dash ~/.config/chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
