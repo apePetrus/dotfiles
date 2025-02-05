c47fd8fa64a861fec3b3335f3ee4a8f7f5709364
apePetrus <shiveringveteran@proton.me>
2025-02-05 17:11:26 -0300

    Adjusted monitor refresh rate through xrandr on run.sh for chadwm

--- chadsuckless/chadwm/scripts/run.sh
+++ chadsuckless/chadwm/scripts/run.sh
@@ -2,6 +2,7 @@
 
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 75
 feh --no-fehbg --bg-fill "$HOME/.feh-bgs/think.jpg" &
 xset r rate 200 50 &
 picom &
