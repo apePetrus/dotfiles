b5ce1bd1d805de716449307a953c9f749e3621ee
apePetrus <shiveringveteran@proton.me>
2025-03-23 19:19:00 -0300

    Changed st font and added dunst for notification center

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "CaskaydiaMono Nerd Font:pixelsize=16";
+static char *font = "CaskaydiaCoveNerdFontMono:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
