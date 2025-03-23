1869dc64c5f3a69443ddde1fa3a112ee21a5be5a
apePetrus <shiveringveteran@proton.me>
2025-03-23 13:53:28 -0300

    Changed the tags system

--- chadsuckless/chadwm/chadwm/config.def.h
+++ chadsuckless/chadwm/chadwm/config.def.h
@@ -114,7 +114,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static char *tags[] = {"一", "二", "三", "四", "五"};
 
 static const char *eww[] = {"eww", "open", "eww", NULL};
 
