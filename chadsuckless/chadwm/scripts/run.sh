29880e3bcc89698d6342b1a34bb7514959b54cfc
apePetrus <shiveringveteran@proton.me>
2025-03-23 13:40:45 -0300

    Changed some bar settings

--- chadsuckless/chadwm/scripts/run.sh
+++ chadsuckless/chadwm/scripts/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
+xrdb merge ~/.Xresources
 xbacklight -set 10 &
-xrandr --output HDMI-A-0 --mode 1920x1080 --rate 75
+# xrandr --output HDMI-A-0 --mode 1920x1080 --rate 75
 feh --no-fehbg --bg-fill "$HOME/.feh-bgs/think.jpg" &
 xset r rate 200 50 &
 picom &
 setxkbmap br &
 pipewire &
 
-dash ~/.config/chadwm/scripts/bar.sh &
+zsh ~/.config/chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
