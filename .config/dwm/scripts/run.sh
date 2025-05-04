532b8eabb62554ce32e129462955dfc9b5ad1cf7
apePetrus <shiveringveteran@proton.me>
2025-05-04 11:29:45 -0300

    Changed feh-bgs location

--- .config/dwm/scripts/run.sh
+++ .config/dwm/scripts/run.sh
@@ -3,7 +3,7 @@
 xrdb merge ~/.Xresources
 xbacklight -set 10 &
 # xrandr --output HDMI-A-0 --mode 1920x1080 --rate 75
-feh --no-fehbg --bg-fill "$HOME/.feh-bgs/girl-and-cat-walking-through-city.jpg" &
+feh --no-fehbg --bg-fill "$HOME/.local/share/feh-bgs/girl-and-cat-walking-through-city.jpg" &
 xset r rate 200 50 &
 picom &
 setxkbmap br &
