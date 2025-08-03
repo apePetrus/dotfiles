80ff3365f6ca64ea407860444a699a613eb9d4ec
Pedro Pereira <pedro.hr.pereira@proton.me>
2025-08-02 22:05:40 -0300

    updated installation process

--- .other/setup/postinstall.sh
+++ .other/setup/postinstall.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # necessary pkgs
-sudo xbps-install -S $(cat ./voidpkgs.txt)
-
-# daemons
-sudo ln -s /etc/sv/dbus /var/service/
-sudo ln -s /etc/sv/elogind /var/service
-sudo ln -s /etc/sv/polkitd /var/service
-
-# touchpad
-sudo mkdir -p /etc/X11/xorg.conf.d/
-sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
-
-# pipewire
-sudo mkdir -p /etc/pipewire/pipewire.conf.d
-sudo mkdir -p /etc/alsa/conf.d
+# sudo xbps-install -S $(cat ./voidpkgs.txt)
+#
+# # daemons
+# sudo ln -s /etc/sv/dbus /var/service/
+# sudo ln -s /etc/sv/elogind /var/service
+# sudo ln -s /etc/sv/polkitd /var/service
+#
+# # touchpad
+# sudo mkdir -p /etc/X11/xorg.conf.d/
+# sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
+#
+# # pipewire
+# sudo mkdir -p /etc/pipewire/pipewire.conf.d
+# sudo mkdir -p /etc/alsa/conf.d
 
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d
@@ -22,9 +22,9 @@ sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
 
 # .config, .themes, .icons, .xinitrc, scripts, feh-bgs and zsh files
-cp -rf ./home-dir/. /home/petrus/
+# cp -rf ./home-dir/. /home/petrus/
 
 # zsh and p10k
-mkdir -p /home/petrus/.oh-my-zsh/custom/themes
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/petrus/.oh-my-zsh/custom/themes/powerlevel10k
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# mkdir -p /home/petrus/.oh-my-zsh/custom/themes
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/petrus/.oh-my-zsh/custom/themes/powerlevel10k
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
